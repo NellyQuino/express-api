3493d05740dcd3384e82828c5751dd09d867e778
NellyQuino <71736979+NellyQuino@users.noreply.github.com>
2022-04-29 15:42:49 -0700

    8. Creted DELETE enpoint

--- app.js
+++ app.js
@@ -39,6 +39,13 @@ app.put('/v1/explorers/:id', (req, res) => {
     const requestBody = req.body;
     res.status(201).json({ message: "Updated" })
 });
+//DELETE ENPOINT GIT 
+app.delete('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers PUT request ${new Date()}`)
+    console.log(`Delete explorer with id ${req.params.id}`)
+    const requestBody = req.body;
+    res.status(200).json({ message: "Deleted" })
+});
 // Con esto inicializamos esta app
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
