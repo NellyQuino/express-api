476be8de2e62bcc89ad0ba1b38643e548bc0d361
NellyQuino <71736979+NellyQuino@users.noreply.github.com>
2022-04-29 15:19:09 -0700

    5. Endpoint para el método GET

--- app.js
+++ app.js
@@ -6,7 +6,26 @@ app.use(express.json()) // Indicamos que usaremos JSON
     // Puerto en que vamos a ver nuestra app: localhost:3000
 const port = 3000
 
+app.get('/v1/explorers', (req, res) => {
+    console.log(`API Explorer GET ALL requests ${new Date()}`)
+    const explorer1 = { id: 1, name: "Nelly1" }
+    const explorer2 = { id: 2, name: "Nelly2" }
+    const explorer3 = { id: 3, name: "Nelly3" }
+    const explorer4 = { id: 4, name: "Nelly4" }
+    const explorers = { explorer1, explorer2, explorer3, explorer4 }
+    res.status(200).json(explorers)
+});
+//Endpoint get->Reegresar informacion 
+
+app.get('/v1/explorers.:id', (req, res) => {
+    console.log(`Api Explorers GET request ${new Date()}`)
+    console.log(`Getting explorer with id ${req.params.id}`)
+
+    const explorer = { id: 1, name: "Nelly" }
+    res.status(200).json(explorer)
+})
+
 // Con esto inicializamos esta app
 app.listen(port, () => {
-            console.log(`Example app listening on port ${port}`)
-        }
+    console.log(`Example app listening on port ${port}`)
+});
